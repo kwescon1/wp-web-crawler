92dc40760adf4a01c42037000874e922283bd7a4
Odame <kwesi.danquah@daaseng.com>
2023-08-26 21:34:15 +0100

    Implement Result Model for Data Representation and Interaction

---
+++ app/Models/Result.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+class Result {
+    private int $id;
+    private string $url;
+
+    public const TABLE = 'crawl_results';
+
+    public function getId(): int{
+        return $this->id;
+    }
+
+    public function getUrl(): string {
+        return $this->url;
+    }
+}
