fc58a62b22062a083e1d3469c6c49499d6dcb628
Odame <kwesi.danquah@daaseng.com>
2023-08-26 21:32:21 +0100

    Implemented a user model class

---
+++ app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+class User {
+    private $id;
+    private $username;
+
+    public const TABLE = 'users';
+
+    public function getId(): int{
+        return $this->id;
+    }
+
+    public function getUsername(): string {
+        return $this->username;
+    }
+}
