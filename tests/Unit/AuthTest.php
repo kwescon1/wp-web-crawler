4221935e19e623754c0c213df045ceed7710d98b
Odame <kwesi.danquah@daaseng.com>
2023-08-29 01:38:57 +0100

    Implemented auth test for user authentication.

---
+++ tests/Unit/AuthTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Services\Auth\AuthService;
+use App\Services\User\UserService;
+use PHPUnit\Framework\TestCase as Test;
+
+class AuthTest extends Test
+{
+
+    protected $testCase;
+    protected $authService;
+    protected $userService;
+
+    protected function setUp(): void
+    {
+
+        parent::setUp();
+
+        $this->testCase = new TestCase;
+        $this->userService = new UserService;
+        $this->authService = new AuthService($this->userService);
+
+        // Reset the database
+        $this->testCase->refreshDatabase();
+    }
+
+    public function testSuccessfulUserLogin()
+    {
+        $username = 'user';
+        $password = 'test_password';
+
+        $this->insertUser($username, password_hash($password, PASSWORD_DEFAULT));
+
+        // Call the method to be tested
+        $result = $this->authService->login($username, $password);
+
+        $this->assertTrue($result);
+
+        $userIsLoggedIn = $this->authService->isLoggedIn();
+
+        $this->assertTrue($userIsLoggedIn);
+    }
+
+    public function testUnsuccessfulUserLogin()
+    {
+
+        $username = 'user';
+        $password = 'test_password';
+
+        $this->insertUser($username, $password);
+
+        // Call the method to be tested
+        $result = $this->authService->login($username, $password);
+
+        $this->assertFalse($result);
+    }
+
+    private function insertUser($username, $password)
+    {
+        $insertQuery = "INSERT INTO " . User::TABLE . " (username, password) VALUES (:username, :password)";
+
+        $stmt = $this->testCase->db()->prepare($insertQuery);
+
+        $stmt->bindValue(':username', $username, $this->testCase->db()::PARAM_STR);
+        $stmt->bindValue(':password', $password, $this->testCase->db()::PARAM_STR);
+        $stmt->execute();
+    }
+}
