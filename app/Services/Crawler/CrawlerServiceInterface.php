5a638262a8deaace6be8f089803cfc08ff9f2a3d
Odame <kwesi.danquah@daaseng.com>
2023-09-04 03:31:32 +0100

    Configured crawler service interface

--- app/Services/Crawler/CrawlerServiceInterface.php
+++ app/Services/Crawler/CrawlerServiceInterface.php
@@ -2,8 +2,13 @@
 
 namespace App\Services\Crawler;
 
-interface CrawlerServiceInterface {
-	public function isValidUrl( string $url) : bool;
+interface CrawlerServiceInterface
+{
 
-	public function crawlHomePage( string $url) :? array;
+    public const CRAWL_CAHCE_KEY = 'crawl';
+    public const CRAWL_CACHE_SECONDS = 3600 * 24;
+
+    public function isValidUrl(string $url): bool;
+
+    public function crawlHomePage(string $url): ?array;
 }
