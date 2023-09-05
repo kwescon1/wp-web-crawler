14f206be81f732ac33b7adcfe67d99b78937b438
Odame <kwesi.danquah@daaseng.com>
2023-09-05 13:13:22 +0100

    feat(tests): Add authentication tests for login page and failed login scenario

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+use Tests\Traits\RefreshDatabase;
+
+class AuthTest extends TestCase
+{
+    // use RefreshDatabase;  // Use the RefreshDatabase trait
+
+    protected $baseUrl;
+    protected $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->baseUrl = 'http://wp_webserver';
+        $this->client = new Client(['base_uri' => $this->baseUrl]);
+    }
+
+    public function testLoginPageIsAccessible()
+    {
+        $response = $this->client->get('/login.php');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('Log in', (string)$response->getBody());
+    }
+
+    public function testFailedLogin()
+    {
+        $response = $this->client->post('/login.php', [
+            'form_params' => [
+                'username' => 'invalid_username',
+                'password' => 'invalid_password',
+            ],
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('Invalid credentials. Please try again.', (string)$response->getBody());
+    }
+}
